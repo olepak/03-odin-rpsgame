905b79ebb956b9123675437bb0e6a1c062cf3e64
oleksandr <oleksandr@Ubuntu.myguest.virtualbox.org>
2023-06-28 16:25:20 +0300

    make function that takes user input and show basic messages

--- script.js
+++ script.js
@@ -9,30 +9,41 @@ const possibleVariant = [
 ];
 function getComputerChoice(variant) {
 	let randomNumber = Math.random();
-	console.log(randomNumber);
-
 	let index = Math.floor(randomNumber * variant.length);
-	console.log(index);
-
+	console.log(variant[index]);
 	return variant[index];
 }
-let result = getComputerChoice(possibleVariant);
-console.log(result);
 /* 2. Write a function that plays a single round
       The function should take two parameters - playerSelection
 			and computerSelection.
 			 then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 			 */
-function singleRound(playerSelection, computerSelection) {}
-function showLoose() {
-	alert("You loose");
+function singleRound(playerSelection, computerSelection) {
+	alert(playerSelection);
+	alert(computerSelection);
+	alert(
+		showWhoBeatsWho(playerSelection, computerSelection) + " " + showLoose()
+	);
 }
+function showLoose() {
+	let message = "You loose";
+	return message;
+}
+
 function showWin() {
 	alert("You won");
 }
 function showEqual() {
 	alert("It is equal");
 }
+function showWhoBeatsWho(winner, looser) {
+	let message = winner + " beats " + looser;
+	return message;
+}
+singleRound(
+	prompt("Please enter scissors, paper or rock"),
+	getComputerChoice(possibleVariant)
+);
 /* 3. Write a new function called game() which plays a five rounds,
       keeps score and reports a winner or a looser at the end.
 */
